ba6f3e6414359499497867589bdd74af4a662630
Jonny <royalpython2@hotmail.com>
2021-11-22 21:44:32 +0000

    Some additional info from a recent bare metal wireguard install

--- wireguardadduser.sh
+++ wireguardadduser.sh
@@ -4,9 +4,10 @@
 #my wireguard uses the 10.6.0.0/24 subnet, so you'll have to update this accordingly.
 #to save myself some typing time, for the $num variable, i just increment by 1 and this is the last digit of the IP.
 #check the wg0.conf file to see which clients are using which IP's.
-#also update your endpoint ip address, or dns (i use dynamic dns) for this.
-#choose which DNS you'd like to use also.
-#lastly, your public key will be your server_pub key.
+#also update your endpoint ip address, or dns (i use dynamic dns) for this,line 23
+#choose which DNS you'd like to use line 23
+#lastly, you'll need the server keys, and the publickey which is added to line 23.
+##umask 077; wg genkey | tee privatekey | wg pubkey > publickey
 
 read -p "Enter peer name: " name
 read -p "Enter subnet last digit: " num
@@ -19,7 +20,7 @@ psk=$(cat $name'_psk')
 
 touch /etc/wireguard/configs/$name'.conf'
 
-echo -e "[Interface]\nPrivateKey = $priv\nAddress = 10.6.0.$num/24\nListenPort = 51820\nDNS = 9.9.9.9\n[Peer]\nPublicKey = XXXXXXXXXXXXXXXXXXXX\nPresharedKey = $psk\nEndpoint = MYDNS.ddns.net:51820\nAllowedIPs = 0.0.0.0/0, ::0/0" >> /etc/wireguard/configs/$name'.conf'
+echo -e "[Interface]\nPrivateKey = $priv\nAddress = 10.6.0.$num/24\nListenPort = 51820\nDNS = 9.9.9.9\n[Peer]\nPublicKey = XXXXXXXXXXXXXXXXXXXX\nPresharedKey = $psk\nEndpoint = MYDNS.ORMY.IP:51820\nAllowedIPs = 0.0.0.0/0, ::0/0" >> /etc/wireguard/configs/$name'.conf'
 
 pub=$(cat $name'_pub')
 echo -e "###$name###\n[peer]\nPublicKey = $pub\nPresharedKey = $psk\nAllowedIPs = 10.6.0.$num/32\n###end $name###" >> /etc/wireguard/wg0.conf
@@ -27,3 +28,12 @@ echo -e "###$name###\n[peer]\nPublicKey = $pub\nPresharedKey = $psk\nAllowedIPs 
 systemctl restart wg-quick@wg0
 
 echo "$name has been added to wireguard,config file located /etc/wireguard/configs"
+
+######Some configurations######
+#ufw route allow in on wg0 out on eth0 from 10.6.0.0/24
+#nano /etc/sysctl.conf and uncomment net.ipv4.ip_forward=1
+#add the below 3 lines to [interface] of wg0.conf
+########
+#SaveConfig = true
+#PostUp = iptables -A FORWARD -i wg0 -j ACCEPT; iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
+#PostDown = iptables -D FORWARD -i wg0 -j ACCEPT; iptables -t nat -D POSTROUTING -o eth0 -j MASQUERADE
