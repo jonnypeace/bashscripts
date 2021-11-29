d7d18e3eb8b07a361e991ff16f5c842810a658c3
Jonny <royalpython2@hotmail.com>
2021-11-29 02:43:02 +0000

    Update mediumerror.sh

--- mediumerror.sh
+++ mediumerror.sh
@@ -17,6 +17,7 @@ if [[ $ans =~ ^(yes|y)$ ]]; then
 	for ((i=$start; i<=$last; i++)); do
 	smartctl -a /dev/pass$i | grep "Serial number" >> serialMediumErrors.txt; smartctl -a /dev/pass$i | grep "Non-medium error" >> serialMediumErrors.txt
 	done
+	exit 0
 fi
 
 for ((i=$start; i<=$last; i++)); do 
