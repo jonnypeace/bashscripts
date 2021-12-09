8f0297382d95e58abd57e4d559cfca0fccd99f9b
Jonny <royalpython2@hotmail.com>
2021-12-09 14:52:37 +0000

    Script should be more user friendly

--- csvTOpass.sh
+++ csvTOpass.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 
-#This is a work in progress for users other than myself. If you understand the code, feel free to modify for your own needs.
 #The purpose for this script is to convert keepass .csv files into pass... a linux password manager which can later be used
 #with passmenu which uses dmenu for passwords - overall, i find pass, passmenu (with dmenu) and pass otp aids in a better
-#user experience using keybindings. It takes a bit of configuring with the csv file, and this code patches a few errors which
-#might not work with you. I.e. If you are correctly using long and complicated passwords, with comma's.... my script has been patched
-#to work with only ONE COMMA IN THE PASSWORD.
+#user experience using keybindings. It takes a little bit of configuring with the csv file.....
+#My CSV files are formatted like so...
+# website,user,password
+# google,jonny,password123
+# This script will not work well if you have additional entries.
+# As you'll see below, my CSV file is called forPass.csv and is located /home/user/Documents
+
+echo -e "Is your CSV file formatted like so....\nwebsite,user,password\nLocated in /home/user/Documents\nFile named forPass.csv? [y/n] : "
+read ans
+
+if [[ $ans =~ ^(yes|y)$ ]]; then
 
 data=$HOME/Documents/forPass.csv
 i=1
@@ -26,4 +33,7 @@ for user in "${array[@]}"
   newpass=$( sed -e 's/,/ /1' -e 's/,/ /1' <<< "${array[i]}" | awk '{print $3}' )
   (echo "$newpass"; echo "$newpass") | pass add --echo -e "$newsite/$newuser"
   i=$(( i + 1 ))
-done
+done; else
+
+echo "Script stopped until CSV is formatted correctly"
+fi
