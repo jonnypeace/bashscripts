4fd018a151f3d587404a9fd7f6d28698a26eeea3
Jonny <royalpython2@hotmail.com>
2022-03-03 11:46:19 +0000

    Updated script with some attribute checks

--- rsynchealth.sh
+++ rsynchealth.sh
@@ -5,19 +5,36 @@
 #the output.
 
 ###############################
-# THIS SCRIPT NEEDS TO INCLUDE SOME MORE DATA FOR CHECKING SAS DRIVES... AND MAYBE SATA, IT'S A WORK IN PROGRESS.
+# AT PRESENT, THIS SCRIPT CHECKS A FEW ATTRIBUTES AND OVERALL HEALTH OF SATA DRIVES.
 ###############################
 
-#Set up the drives for synchronizing. I use /dev/sda as my shared network drive - NEITHER DRIVE IS MY ROOT DIRECTORY.
-#I've only tested on storage drives outside the root file system. This checks smartmontools for the overall-health of the drives.
-n=$(sudo smartctl -a /dev/sda | grep "PASSED")
-m=$(sudo smartctl -a /dev/sdb | grep "PASSED")
-if [[ ( $n == "SMART overall-health self-assessment test result: PASSED" ) && ( $m == "SMART overall-health self-assessment test result: PASSED" ) ]]
-#then echo "GOOD HEALTH"
-#/media/drive1 and /media/drive2 will be which ever storage drives you've mounted for rsync.
- then
- rsync -ahv --delete-after /media/drive1 /media/drive2
- echo "sync completed"
- else 
- echo "BAD HEALTH"
+# Checking health of drive sda
+a_pass=$(sudo smartctl -H /dev/sda | awk 'NR==5{print $6}')
+a_pend=$(sudo smartctl -A /dev/sda | awk '/Current_Pending_Sector/{print $10}')
+a_reall=$(sudo smartctl -A /dev/sda | awk '/Reallocated_Event_Count/{print $10}')
+a_uncor=$(sudo smartctl -A /dev/sda | awk '/Offline_Uncorrectable/{print $10}')
+
+# Checking health of drive sdb
+b_pass=$(sudo smartctl -H /dev/sdb | awk 'NR==5{print $6}')
+b_pend=$(sudo smartctl -A /dev/sdb | awk '/Current_Pending_Sector/{print $10}')
+b_reall=$(sudo smartctl -A /dev/sdb | awk '/Reallocated_Event_Count/{print $10}')
+b_uncor=$(sudo smartctl -A /dev/sdb | awk '/Offline_Uncorrectable/{print $10}')
+
+# Testing whether the drives are mounted
+testmount1=$(df -h | awk '/\/drive1/{print $6}')
+testmount2=$(df -h | awk '/\/drive2/{print $6}')
+
+# If drives are not mounted, script will exit.
+if [[ -e $testmount1 || -e $testmount2 ]] ; then
+  echo drive sda & sdb mounted and testing...
+# Checking drive health and attributes are in good condition  
+  if [[ $a_pass != PASSED || $a_pend != 0 || $a_reall != 0 || $a_uncor != 0 || $b_pass != PASSED || $b_pend != 0 || $b_reall != 0 || $b_uncor != 0 ]] ; then
+# If one of the drives does not conform to the above arguments, no rsync takes place
+    echo "Check drive health, might be time to replace one of the drives" ; else
+# If both drives pass the arguments, rsync will progress.
+# IMPORT when using rsync. Always format like so.... rsync -options /drive1/ /drive2/ with the slash at the end of your mount point or directory.
+    echo "Syncing drives..."
+    rsync -avhrxH --delete /drive1/ /drive2/
+  fi ; else
+  echo "Check drives are mounted correctly, or if drives have failed"
 fi
