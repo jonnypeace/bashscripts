a2012ebb0ae92c83f1f2012db25dcdf3f3c09aaf
Jonny <royalpython2@hotmail.com>
2022-05-15 16:51:43 +0100

    Update growlist.sh

--- growlist.sh
+++ growlist.sh
@@ -11,30 +11,30 @@ smartctl --scan
 read -p "Start drive number, i.e. /dev/pass2. 2 would be the start number: " start
 read -p "Last drive number, i.e /dev/pass5. 5 would be the last number: " last
 
-if [[ $ans =~ ^(yes|y)$ ]]; then
+if [[ "$ans" =~ ^(yes|y)$ ]]; then
 	cat /dev/null > growlist.txt
-	for ((i=$start; i<=$last; i++)); do
-	smartctl -a /dev/pass$i | grep "Serial number" >> growlist.txt; smartctl -a /dev/pass$i | grep -i "Elements in grown defect list" >> growlist.txt
+	for ((i="$start"; i<="$last"; i++)); do
+	smartctl -a /dev/pass"$i" | grep "Serial number" >> growlist.txt; smartctl -a /dev/pass"$i" | grep -i "Elements in grown defect list" >> growlist.txt
 	done
 	exit 0
 fi
 
-for ((i=$start; i<=$last; i++)); do 
-	smartctl -a /dev/pass$i | grep "Serial number" >> $growtemp; smartctl -a /dev/pass$i | grep -i "Elements in grown defect list" >> $growtemp
+for ((i="$start"; i<="$last"; i++)); do 
+	smartctl -a /dev/pass"$i" | grep "Serial number" >> "$growtemp"; smartctl -a /dev/pass"$i" | grep -i "Elements in grown defect list" >> "$growtemp"
 done
 
 #Check previous disk health
-maxnum=$(( ($last-$start+1)*2 ))
-for ((i=$start; i<=$maxnum; i=i+2)); do
+maxnum=$(( ("$last"-"$start"+1)*2 ))
+for ((i="$start"; i<="$maxnum"; i=i+2)); do
 	disk=$(awk "NR==$i"'{print $6}' growlist.txt)
-	diskC=$(awk "NR==$i"'{print $6}' $growtemp)
-	scalc=$(( $i-1 ))
+	diskC=$(awk "NR==$i"'{print $6}' "$growtemp")
+	scalc=$(( "$i"-1 ))
 	serial=$(awk "NR==$scalc"'{print $3}' growlist.txt)
-	if [[ $disk -lt $diskC ]]; then
+	if [[ "$disk" -lt "$diskC" ]]; then
 		diff=$(( "$diskC" - "$disk" ))
 		echo "***** disk $serial Elements in Grown Defect List has increased by $diff *****"; else
 		echo "disk $serial Grown Defect List are ok"
 	fi
 done
 
-rm $growtemp
+rm "$growtemp"
