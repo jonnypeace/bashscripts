5565fcf0fe61cd219d58478a1e8beb3b16e7e5d9
jonnypeace <royalpython2@hotmail.com>
2022-05-15 17:31:11 +0100

    run through shellcheck for errors

--- sha256.sh
+++ sha256.sh
@@ -8,8 +8,8 @@
 #name of the iso you want to check,i.e. ./sha256.sh proxmox.4.5.6.iso (the iso will be $1)
 #copy the sha256sum text from the website to check against.
 
-read -p "iso text from website to check against: " check
-sha=$(sha256sum -b $1 | cut -d' ' -f1)
+read -rp "iso text from website to check against: " check
+sha=$(sha256sum -b "$1" | cut -d' ' -f1)
 if [ "$check" = "$sha" ]
 	then echo "ISO check passed"
 	else echo "ISO check failed"
