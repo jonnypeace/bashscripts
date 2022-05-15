c2096bd12b18761a0f56ab64dc6a9dc8106f05fb
jonnypeace <royalpython2@hotmail.com>
2022-05-15 17:26:39 +0100

    Run through shellcheck for errors

--- evenRound.sh
+++ evenRound.sh
@@ -1,17 +1,17 @@
-##!/bin/bash
+#!/bin/bash
 
 # ASTM requires rounding numbers even. This bash script can be used as a function inside other larger 
 # mathematical scripts to round numbers to even, i,e. 0.5 = 0 , 0.15 = 0.2 etc.
 
 prune1=$(echo "scale=$2+1;$1/1" | bc )
-last2dig=$(echo $prune1 | sed -e 's/\(^.*\)\(..$\)/\2/')
+last2dig=$(echo "$prune1" | sed -e 's/\(^.*\)\(..$\)/\2/')
 div=$(echo "scale=$2+1;5/10^($2+1)" | bc )
 mod=$(echo "$1%$div" | bc )
 
 if [ "$last2dig" == ".5" ];
 then
   last2dig=$(echo "$1*10" | bc | cut -d "." -f1 | sed -e 's/\(^.*\)\(..$\)/\2/' )
-  if [ $last2dig == 5 ]
+  if [ "$last2dig" == 5 ]
   then
     last2dig="05"
   fi
@@ -20,7 +20,9 @@ fi
 echo "prune1=$prune1 last2dig=$last2dig mod=$mod div=$div"
 if [ "$last2dig" == 05 ] || [ "$last2dig" == 25 ] || [ "$last2dig" == 45 ] || [ "$last2dig" == 65 ] || [ "$last2dig" == 85 ] && [ "$mod" == 0 ];
 then
-  echo $(echo "scale=$2;$1/1" | bc );
+  #echo $(echo "scale=$2;$1/1" | bc );
+  echo "scale=$2;$1/1" | bc ;
 else
-  echo $(echo "scale=$2;(((10^$2)*$1)+0.5)/(10^$2)" | bc );
+  #echo $(echo "scale=$2;(((10^$2)*$1)+0.5)/(10^$2)" | bc );
+  echo "scale=$2;(((10^$2)*$1)+0.5)/(10^$2)" | bc;
 fi
