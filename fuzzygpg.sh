6ad0c896c1388611743e9b7d0cd75ae9d23fd521
jonnypeace <royalpython2@hotmail.com>
2022-08-19 12:00:55 +0100

    quality of life improvements with fzf

--- fuzzygpg.sh
+++ fuzzygpg.sh
@@ -12,6 +12,7 @@
 temp1=$(mktemp -t test1.XXXXXX)
 temp2=$(mktemp -t test2.XXXXXX)
 
+# trap to stop your encryption keys being exposed, and tidy temporary files
 function for-trap {
 
 for files in "$PWD"/cryptconfig/*
@@ -27,10 +28,10 @@ exit
 
 trap 'for-trap' 2
 
-function yes-no-contin {
-	ans=$(printf '%s\n' "yes" "no" | fzf --header="Please Select if you want to continue" --margin=10%)
-	return 
-}
+#function yes-no-contin {
+#	ans=$(printf '%s\n' "yes" "no" | fzf --header="Please Select if you want to continue" --margin=10%)
+#	return 
+#}
 
 # create new encrypted file for mounting
 function createnew {
@@ -38,14 +39,18 @@ function createnew {
 config="$PWD/cryptconfig"
 mkdir -p "$config"
 
-dialog --title "Create New Crypto" \
+dialog --no-shadow --title "Create New Crypto" \
 --form "\n\nCtrl+c will unmount and delete decrypted file" 30 60 10 \
   "Encrpyted File Name" 1 1 "" 1 37 15 50 \
   "byte size, i.e. 1M? (for dd)" 2 1 "" 2 37 15 50 \
   "Count, i.e. 1024 ? (for dd)" 3 1 "" 3 37 15 50 \
   "Full Mount Point i.e. /mnt/crypto" 4 1 "" 4 37 15 50 \
 2>"$temp2"
-if [ $? == 1 ]; then return ; fi
+
+if [ $? != 0 ]
+then
+	return
+fi
 
 enfile=$(awk 'NR==1{print}' "$temp2")
 bsize=$(awk 'NR==2{print}' "$temp2")
@@ -53,6 +58,7 @@ count=$(awk 'NR==3{print}' "$temp2")
 mount=$(awk 'NR==4{print}' "$temp2")
 
 cp "$temp2" "$config/$enfile"
+echo "$(( ${bsize%?} * count )) MB file size" >> "$config/$enfile"
 
 sudo mkdir -p "$mount"
 clear
@@ -68,8 +74,8 @@ then
 fi
 
 if [[ $? != 0 ]]; then
-  dialog --msgbox "Unable to proceed with encryption" 25 60; else
-  dialog --msgbox "File $enfile now encrypted, to mount choose mount existing from main menu" 25 60
+  dialog --no-shadow --msgbox "Unable to proceed with encryption" 25 60; else
+  dialog --no-shadow --msgbox "\nFile $enfile now encrypted\n\nTo mount file choose option 2 from main menu, for 'mount existing crypto file'\n\nFile located in $config" 25 60
 fi
 
 }
@@ -79,7 +85,7 @@ function existcrypt {
 
 config="$PWD/cryptconfig"
 list=$(find "$config" -type f | fzf --header="
-Select/Search(type) and press ENTER: " --margin=20% --preview "cat {}")
+Select/Search(type) and press ENTER: " --margin=20% --preview-window=up,25% --preview "cat {}")
 
 if [[ ! $list ]] ; then return ; fi
 
@@ -94,8 +100,8 @@ wait
 sudo mount "$enfile" "$mount"
 
 if [[ $? != 0 ]]; then
-  dialog --msgbox "Unable to proceed, check file location and script path\n\nScript will run best in a dedicated crypto directory in your $HOME" 25 60; else
-  dialog --msgbox "File mounted in $mount" 25 60
+  dialog --no-shadow --msgbox "Unable to proceed, check file location and script path\n\nScript will run best in a dedicated crypto directory in your $HOME" 25 60; else
+  dialog --no-shadow --msgbox "File mounted in $mount" 25 60
 fi
 clear
 
@@ -105,8 +111,8 @@ clear
 function unmount {
 
 config="$PWD/cryptconfig"
-list=$(find "$config" -type f | fzf --header="i
-Select/Search(type) and press ENTER: " --margin=20% --preview "cat {}")
+list=$(find "$config" -type f | fzf --header="
+Select/Search(type) and press ENTER: " --margin=20% --preview-window=up,25% --preview "cat {}")
 if [[ ! $list ]] ; then return ; fi
 enfile=$(awk 'NR==1{print}' "$list")
 mount=$(awk 'NR==4{print}' "$list")
@@ -124,8 +130,8 @@ then
 fi
 
 if [[ $? != 0 ]]; then
-  dialog --msgbox "Unable to proceed, check file location, are you sudo" 25 60; else
-  dialog --msgbox "File has been unmounted and encrypted" 25 60
+  dialog --no-shadow --msgbox "Unable to proceed, check files and locations?" 25 60; else
+  dialog --no-shadow --msgbox "File has been unmounted and encrypted" 25 60
 fi
 
 }
@@ -133,11 +139,11 @@ fi
 # while loop for main dialog window
 while true
   do
-	  dialog --no-shadow --menu "Crypto Files\nCtrl+c will unmount and delete decrypted file" 30 60 10 1 "Create New?" 2 "Mount Existing Crypto File?" 3 "Save & Unmount?" 0 "Exit Completely (mounts will still be mounted)" 2>"$temp1"
-  #if [[ $? != 0 ]]
-  #then
-  #  break
-  #fi
+	  dialog --no-shadow --menu "Crypto Files\nCtrl+c will unmount and delete decrypted file" 30 60 10 1 "Create New encryption file?" 2 "Mount Existing Crypto File?" 3 "Save & Unmount?" 0 "Exit Completely (mounts will still be mounted)" 2>"$temp1"
+  if [[ $? != 0 ]]
+  then
+    break
+  fi
 
   selection=$(cat "$temp1")
   case "$selection" in
