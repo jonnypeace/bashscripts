e0d49b3147e25d7a578d5f5acf635e5884ab8c7b
jonnypeace <royalpython2@hotmail.com>
2022-08-21 17:42:00 +0100

    Some minor improvements, using EOF and new comments for UFW

--- wireguardadduser.sh
+++ wireguardadduser.sh
@@ -4,9 +4,9 @@
 #my wireguard uses the 10.6.0.0/24 subnet, so you'll have to update this accordingly.
 #to save myself some typing time, for the $num variable, i just increment by 1 and this is the last digit of the IP.
 #check the wg0.conf file to see which clients are using which IP's.
-#also update your endpoint ip address, or dns (i use dynamic dns) for this,line 23
-#choose which DNS you'd like to use line 23
-#lastly, you'll need the server keys, and the publickey which is added to line 23.
+#also update your endpoint ip address, or i use dynamic dns for this,line 38 
+#choose which DNS you'd like to use line 34
+#lastly, you'll need the publickey from your server which is added to line 36 - use comment below to generate your server keys.
 ##umask 077; wg genkey | tee privatekey | wg pubkey > publickey
 
 #ubuntu server doesn't allow the edit of the wg0.conf file unless wireguard service has stopped running.
@@ -15,38 +15,64 @@ mkdir -p /etc/wireguard/configs
 
 if [[ "$ans" =~ ^(yes|y)$ ]]; then
 
-  systemctl stop wg-quick@wg0
-  read -rp "Enter peer name: " name
-  read -rp "Enter subnet last digit, i.e. 10.6.0.2, last digit being 2: " num
-  umask 077
-  wg genkey | tee "$name"'_priv' | wg pubkey > "$name"'_pub'
-  umask 077
-  wg genpsk > "$name"'_psk'
-  priv=$(cat "$name"'_priv')
-  psk=$(cat "$name"'_psk')
+	systemctl stop wg-quick@wg0
+	read -rp "Enter peer name: " name
+	read -rp "Enter subnet last digit, i.e. 10.6.0.2, last digit being 2: " num
+	umask 077
+	wg genkey | tee "$name"'_priv' | wg pubkey > "$name"'_pub'
+	wg genpsk > "$name"'_psk'
+	priv=$(cat "$name"'_priv')
+	psk=$(cat "$name"'_psk')
 
-  touch /etc/wireguard/configs/"$name".conf
+	touch /etc/wireguard/configs/"$name".conf
 
-  echo -e "[Interface]\nPrivateKey = $priv\nAddress = 10.6.0.$num/24\nListenPort = 51820\nDNS = 9.9.9.9\n[Peer]\nPublicKey = MYPUBKEY\nPresharedKey = $psk\nEndpoint = MYDNS.ORMY.IP:51820\nAllowedIPs = 0.0.0.0/0, ::0/0" >> /etc/wireguard/configs/"$name"'.conf'
+	cat <<- EOF >> /etc/wireguard/configs/"$name".conf
+	[Interface]
+	PrivateKey = $priv
+	Address = 10.6.0.$num/24
+	ListenPort = 51820
+	DNS = 9.9.9.9
+	[Peer]
+	PublicKey = MYPUBKEY
+	PresharedKey = $psk
+	Endpoint = MYDNS.ORMY.IP:51820
+	AllowedIPs = 0.0.0.0/0, ::0/0
+	EOF
 
-  pub=$(cat "$name"_pub)
-  echo -e "###$name###\n[peer]\nPublicKey = $pub\nPresharedKey = $psk\nAllowedIPs = 10.6.0.$num/32\n###end $name###" >> /etc/wireguard/wg0.conf
+	pub=$(cat "$name"_pub)
 
-  systemctl start wg-quick@wg0
+	cat <<- EOF >> /etc/wireguard/wg0.conf
+	###$name###
+	[peer]
+ 	PublicKey = $pub
+	PresharedKey = $psk
+	AllowedIPs = 10.6.0.$num/32
+	###end $name### 
+	EOF
 
-  echo "$name has been added to wireguard,config file located /etc/wireguard/configs" ; else
-  echo "Exiting Script....."
-  exit 0 ;
+	systemctl start wg-quick@wg0
+
+	echo "$name has been added to wireguard,config file located /etc/wireguard/configs" ; else
+	echo "Exiting Script....."
+	exit 0 ;
 fi
 
 
-######Some configurations######
+######Some configurations for UFW#############################################################################################
 #ufw route allow in on wg0 out on eth0 from 10.6.0.0/24
-#nano -l +28 /etc/sysctl.conf and uncomment net.ipv4.ip_forward=1 for debian it's line 28.
-#add the below 3 lines to [interface] of wg0.conf. This might be optional, i've not needed this with raspbian, but for a Linode Debian server i have
-#needed to add these lines.
-######################################
-#SaveConfig = true (optional? Seems to store your connection IP in config)
-#PostUp = iptables -A FORWARD -i wg0 -j ACCEPT; iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
-#PostDown = iptables -D FORWARD -i wg0 -j ACCEPT; iptables -t nat -D POSTROUTING -o eth0 -j MASQUERADE
-######################################
+#nano /etc/ufw/sysctl.conf and uncomment net.ipv4.ip_forward=1
+#Set up nat routing and edit /etc/ufw/before.rules and include these lines before the filter rules.
+##############################################################################
+#		# nat Table rules
+#		*nat
+#		:POSTROUTING ACCEPT [0:0]
+
+#		# Forward traffic from wg0 through eth0.
+#		-A POSTROUTING -s 10.10.10.0/24 -o eth0 -j MASQUERADE
+
+#		don't delete the 'COMMIT' line or these nat table rules won't be processed
+#		COMMIT
+##############################################################################
+
+#Directly under the MASQUERADE rule, the rule for that table must have COMMIT. This is the case for each table in these rules.
+##############################################################################################################################
